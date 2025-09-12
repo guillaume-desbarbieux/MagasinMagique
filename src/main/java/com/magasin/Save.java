5a7ca873badd8ce9baf8f135b09f6552dd5acdd5
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-09-12 15:55:37 +0200

    Ajout class save

---
+++ src/main/java/com/magasin/Save.java
@@ -0,0 +1,26 @@
+package com.magasin;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Save {
+    private final File dossierData = new File("data");
+    private final File goldenMaster = new File(dossierData, "goldenMaster.txt");
+
+
+    public void saveText(String text){
+        if (!dossierData.exists()) {
+            if (!dossierData.mkdirs()) {
+                System.out.println("Error can't create 'data'.");
+                return;
+            }
+        }
+
+        try (FileWriter writerPlayer = new FileWriter(goldenMaster, true)) {
+            writerPlayer.write(text + "\n");
+        } catch (IOException e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+    }
+}
