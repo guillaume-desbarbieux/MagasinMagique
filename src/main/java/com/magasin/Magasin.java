ca2c5b2aeb6bc1e6a55d26ebea88fcd7579697fd
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-15 12:24:30 +0200

    Refactor : remplacement des conditions imbriquées par un switch pour améliorer la lisibilité et la maintenance dans `updateQuality`

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -1,47 +1,27 @@
 package com.magasin;
-
 class Magasin {
     Item[] items;
-
-    public Magasin(Item[] items) {
-        this.items = items;
-    }
-
+    public Magasin(Item[] items) {this.items = items;}
     public void updateQuality() {
         for (Item item : items) {
             if (item.name.equals("Kryptonite")) continue;
-
             item.sellIn--;
-
-            if (item.name.equals("Comté") || item.name.equals("Pass VIP Concert")) {
-
-                if (item.quality < 50) {
-                    item.quality++;
-
-                    if (item.name.equals("Pass VIP Concert")) {
-
-                        if (item.sellIn < 10 && item.quality < 50)
-                            item.quality++;
-
-                        if (item.sellIn < 5 && item.quality < 50)
-                            item.quality++;
-                    }
-                }
-            } else item.quality--;
-
-
-            if (item.sellIn < 0) {
-
-                if (item.name.equals("Pass VIP Concert"))
-                    item.quality = 0;
-
-                if (item.name.equals("Comté") && item.quality < 50)
-                    item.quality++;
-
-                else item.quality--;
+            switch (item.name) {
+                case "Comté" :
+                    if (item.quality < 50) item.quality++;
+                    if (item.sellIn < 0 && item.quality < 50) item.quality++;
+                    break;
+                case "Pass VIP Concert" :
+                    if (item.quality < 50) item.quality++;
+                    if (item.sellIn < 10 && item.quality < 50) item.quality++;
+                    if (item.sellIn < 5 && item.quality < 50)  item.quality++;
+                    if (item.sellIn < 0) item.quality = 0;
+                    break;
+                default:
+                    item.quality--;
+                    if (item.sellIn < 0) item.quality--;
+                    if (item.quality < 0) item.quality = 0;
             }
-
-            if (item.quality < 0) item.quality = 0;
         }
     }
 }
