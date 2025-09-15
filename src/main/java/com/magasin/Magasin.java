71b02e5ff708130c0c8ce978539f00efc9dd7865
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-15 14:22:25 +0200

    Ajustements dans `updateQuality` : suppression des duplications de limites de qualité, correction des incréments pour "Comté" et "Pass VIP Concert", ajustement simplifié pour "Kryptonite"

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -9,28 +9,33 @@ class Magasin {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Kryptonite")) continue;
             item.sellIn--;
+
             switch (item.name) {
+                case "Kryptonite":
+                    item.sellIn++;
+                    break;
                 case "Comté":
-                    if (item.quality < 50) item.quality++;
-                    if (item.sellIn < 0 && item.quality < 50) item.quality++;
+                    item.quality++;
+                    //if (item.sellIn < 0) item.quality++;
                     break;
                 case "Pass VIP Concert":
-                    if (item.quality < 50) item.quality++;
-                    if (item.sellIn < 10 && item.quality < 50) item.quality++;
-                    if (item.sellIn < 5 && item.quality < 50) item.quality++;
+                    item.quality++;
+                    if (item.sellIn < 10) item.quality++;
+                    if (item.sellIn < 5) item.quality++;
                     if (item.sellIn < 0) item.quality = 0;
                     break;
                 case "Pouvoirs Magiques":
                     item.quality -= 2;
                     if (item.sellIn < 0) item.quality -= 2;
-                    if (item.quality < 0) item.quality = 0;
                     break;
                 default:
-                    if (item.quality > 0) item.quality--;
-                    if (item.sellIn < 0 && item.quality > 0) item.quality--;
+                    item.quality--;
+                    if (item.sellIn < 0) item.quality--;
             }
+
+            if (item.quality < 0) item.quality = 0;
+            if (item.quality > 50) item.quality = 50;
         }
     }
 }
