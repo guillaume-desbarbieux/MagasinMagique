d74669ca25d1faa85256602d4b558f1c492cd4b4
Guillaume Desbarbieux <guillaume.desbarbieux@le-campus-numerique.fr>
2025-09-15 13:23:43 +0200

    Ajout de la prise en charge de l'item "Pouvoirs Magiques" dans `updateQuality` et ajustements mineurs de lisibilité

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -1,26 +1,35 @@
 package com.magasin;
+
 class Magasin {
     Item[] items;
-    public Magasin(Item[] items) {this.items = items;}
+
+    public Magasin(Item[] items) {
+        this.items = items;
+    }
+
     public void updateQuality() {
         for (Item item : items) {
             if (item.name.equals("Kryptonite")) continue;
             item.sellIn--;
             switch (item.name) {
-                case "Comté" :
+                case "Comté":
                     if (item.quality < 50) item.quality++;
                     if (item.sellIn < 0 && item.quality < 50) item.quality++;
                     break;
-                case "Pass VIP Concert" :
+                case "Pass VIP Concert":
                     if (item.quality < 50) item.quality++;
                     if (item.sellIn < 10 && item.quality < 50) item.quality++;
-                    if (item.sellIn < 5 && item.quality < 50)  item.quality++;
+                    if (item.sellIn < 5 && item.quality < 50) item.quality++;
                     if (item.sellIn < 0) item.quality = 0;
                     break;
-                default:
-                    item.quality--;
-                    if (item.sellIn < 0) item.quality--;
+                case "Pouvoirs Magiques":
+                    item.quality -= 2;
+                    if (item.sellIn < 0) item.quality -= 2;
                     if (item.quality < 0) item.quality = 0;
+                    break;
+                default:
+                    if (item.quality > 0) item.quality--;
+                    if (item.sellIn < 0 && item.quality > 0) item.quality--;
             }
         }
     }
